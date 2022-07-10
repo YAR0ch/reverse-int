b6c783f7e0bd91c5020f37333a60279f6b7f4e83
YAR0ch <yaro.ch@mail.ru>
2022-07-10 22:38:38 +0300

    uppdate: create a reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let num=n;
+let temp;
+
+temp = Number(('' + Math.abs(num)).split('').reverse().join(''));
+//if(num <0) return ( Number('-' + temp) )
+//else
+ return (temp);
+
 }
